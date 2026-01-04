2fb30c0b13d2b9d0de6ea2b90fcbf9e3e9e4c89d
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2026-01-04 14:48:04 +0600

    initial-commit

--- template.py
+++ template.py
@@ -6,6 +6,7 @@ project_name = "src"
 list_of_files = [
 
     f"{project_name}/__init__.py",
+    f"{project_name}/config.py",
     f"{project_name}/components/__init__.py",
     f"{project_name}/components/data_ingestion.py",  
     f"{project_name}/components/data_validation.py",
