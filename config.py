e0abc05770603d2fbf774cd5d5479349173c0263
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2026-01-04 14:16:31 +0600

    env-configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+class Settings(BaseSettings):
+    MONGODB_USER : str 
+    MONGODB_PASSWORD : str 
+    model_config = SettingsConfigDict(env_file=".env",extra="ignore")
+ 
+CONFIG = Settings() 
