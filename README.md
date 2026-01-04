51c32a42132aaf8a3ffa754d4b8c52a3f900899c
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2026-01-04 13:44:32 +0600

    initial-commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vehicle_insurance_end_2_end_project
 vehicle insurance end to end project
+
+
+**mongodb username:**
+```text
+yasinarafate2021_db_user
+```
+
+**mongodb password:**
+```text
+1hUsfHur1Ooe4mqY
+```
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    author="Yasin Arafat",
+    author_email="yasinarafat.e2021@gmail.com",
+    packages=find_packages()
+)
